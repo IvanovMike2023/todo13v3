92f21e504c8877b1b401d254473f5d45565522ad
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-01-10 22:43:21 +0300

    get axioa

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,14 +1,15 @@
-
-
 import React, {useEffect, useState} from 'react'
+import axios from "axios";
 
 export default {
     title: 'API'
 }
-
+const baseURL = 'https://social-network.samuraijs.com/api/1.1'
+const config = {withCredentials: true}
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+       const promise = axios.get(`${baseURL}/todo-lists`,config )
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
 
