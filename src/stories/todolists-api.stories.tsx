61ff5a953405a41fff1ecf18f9206eb9c72b0c2a
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2024-01-11 17:28:03 +0300

    get,post.delete axioa

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -10,6 +10,9 @@ export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
        const promise = axios.get(`${baseURL}/todo-lists`,config )
+           .then((res)=>{
+               setState(res.data)
+           })
         // здесь мы будем делать запрос и ответ закидывать в стейт.
         // который в виде строки будем отображать в div-ке
 
@@ -19,6 +22,9 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const title = 'React'
+        const promise = axios.post(`${baseURL}/todo-lists`,{title},config)
+            .then((res)=>setState(res.data))
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -26,6 +32,12 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todolistId= '64114950-d0ef-4788-ab6e-fc022238d014'
+        const promise = axios.delete(`${baseURL}/todo-lists/${todolistId}`,config)
+            .then((res)=> {
+                setState(res.data)
+                console.log(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
